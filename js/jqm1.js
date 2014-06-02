31e3c957cceb16790fe6ef78f05e1f096510c899
Jill Warvel <jwarvel@exacttarget.com>
2014-06-02 16:29:58 -0400

    hide inputs

--- js/jqm1.js
+++ js/jqm1.js
@@ -1,14 +1,7 @@
 $(function(){
     console.log('hey');
-    $('#dateInput').on('change', function(){
-        $('#dateValue').html(  $('#dateInput').val() );
-    });
-    $('#timeInput').on('change', function(){
-        $('#timeValue').html( $('#timeInput').val() );
-    });
 
-    //$('#fuelDatePicker').datepicker();
-
+    // date time picker lauched via button
     $('.changeDateBtn').on('click', function(){
         $('#nativeDateInput').focus();
     });
@@ -16,4 +9,25 @@ $(function(){
     $('.changeTimeBtn').on('click', function(){
         $('#nativeTimeInput').focus();
     });
+
+    $('#nativeDateInput').on('change', function(){
+        $('#nativeDateValue').html(  $('#nativeDateInput').val() );
+    });
+
+    $('#nativeTimeInput').on('change', function(){
+        $('#nativeTimeValue').html( $('#nativeTimeInput').val() );
+    });
+
+    // date picker
+    $('#dateInput').on('change', function(){
+        $('#dateValue').html(  $('#dateInput').val() );
+    });
+
+    //time picker
+    $('#timeInput').on('change', function(){
+        $('#timeValue').html( $('#timeInput').val() );
+    });
+
+    //$('#fuelDatePicker').datepicker();
+
 });
