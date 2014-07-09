bdaa11f03ca5faea0519b72f152c95181f9e342b
Jill Warvel <jwarvel@exacttarget.com>
2014-07-09 11:38:45 -0400

    added post with delay

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ var port = Number(process.env.PORT || 5001);
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
+
+app.post('/', function(req, res) {
+    setTimeout( function(){
+        res.send('this is a test');
+    }, 86400000 );
+});
